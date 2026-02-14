c2af445728b2aadf2fbfdbfc7819246afc7205a3
WintiS <kviteksima@seznam.cz>
2026-02-14 10:34:44 +0100

    fixed import issue

--- pages/api/create-portal-session.js
+++ pages/api/create-portal-session.js
@@ -1,5 +1,5 @@
 import Stripe from 'stripe';
-import admin from '../../../lib/firebase-admin';
+import admin from '../../lib/firebase-admin';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 

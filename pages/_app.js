96c33417276cac8b87fed31a8eeededfe7da629b
WintiS <kviteksima@seznam.cz>
2026-02-24 12:30:01 +0100

    try posthog install manual

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,22 @@ import '../styles/globals.css';
 import { AuthProvider } from '../context/AuthContext';
 import { LanguageProvider } from '../context/LanguageContext';
 import Head from 'next/head';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import posthog from 'posthog-js';
 
 function MyApp({ Component, pageProps }) {
+    const router = useRouter();
+
+    useEffect(() => {
+        // Track pageviews on route changes (Pages Router)
+        const handleRouteChange = () => posthog.capture('$pageview');
+        router.events.on('routeChangeComplete', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, []);
+
     return (
         <>
             <Head>
